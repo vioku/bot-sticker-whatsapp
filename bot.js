da65ccc8dc90ed864f09b737fb60f3ed5b720422
Dendi Rizky Sanjaya <dendirizkysanjaya@gmail.com>
2024-01-25 07:33:43 +0700

    first commit

---
+++ bot.js
@@ -0,0 +1,25 @@
+const qrcode = require("qrcode-terminal");
+const { Client } = require("whatsapp-web.js");
+
+const client = new Client();
+
+client.on("qr", (qr) => {
+  // Generate and scan this code with your phone
+  console.log("QR RECEIVED");
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message", async (msg) => {
+  if (msg.body == "!ping") {
+    msg.reply("pong");
+  } else if (msg.body === "!stiker" || (msg.body === "!sticker" && msg.hasMedia)) {
+    const attachmentData = await msg.downloadMedia();
+    client.sendMessage(msg.from, attachmentData, { sendMediaAsSticker: true, stickerAuthor: "VIO" });
+  }
+});
+
+client.initialize();
